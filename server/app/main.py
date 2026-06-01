f350f295768abf67a16c6fcbb6a473d6f0a22616
Mykael Sicard <112145455+mykaelsss@users.noreply.github.com>
2026-06-01 19:03:11 -0400

    enable CORS for local client origin

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.sessions.routes import router as session_router
 from app.schedule.routes import router as schedule_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(schedule_router, prefix='/schedule')
 app.include_router(session_router, prefix='/sessions')
