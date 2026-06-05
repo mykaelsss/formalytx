ab150ce9a250d33b7a5cef55b4a79fe254dfdd8f
Mykael Sicard <112145455+mykaelsss@users.noreply.github.com>
2026-06-05 14:11:21 -0400

    add size check on useEcharts hook

--- client/lib/hooks/useEcharts.tsx
+++ client/lib/hooks/useEcharts.tsx
@@ -5,17 +5,22 @@ export function useEcharts() {
   const chartRef = useRef<HTMLDivElement>(null);
   
   useEffect(() => {
-    const el = chartRef.current;
-    if (!el) return;
-    const instance = init(el);
-    if (!instance) return;
-    const ro = new ResizeObserver(() => instance.resize());
-    ro.observe(el);
-    return () => {
-      ro.disconnect();
-      instance.dispose();
-    };
-  }, []);
+      const el = chartRef.current;
+      if (!el) return;
+
+      let instance: ReturnType<typeof init> | undefined;
+      const ro = new ResizeObserver(() => {
+        if (el.clientWidth === 0 || el.clientHeight === 0) return;
+        if (!instance) instance = init(el);
+        instance.resize();
+      });
+      ro.observe(el);
+
+      return () => {
+        ro.disconnect();
+        instance?.dispose();
+      };
+    }, []);
 
   return chartRef;
 }
