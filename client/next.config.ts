16778dc97c479bdf518fb3a728dd6f2d0d1a22cc
Mykael Sicard <112145455+mykaelsss@users.noreply.github.com>
2026-06-05 22:47:38 -0400

    remove turbopack root from next.config

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  turbopack: {
-    root: process.cwd(),
-  },
 };
 
 export default nextConfig;
