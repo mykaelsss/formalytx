67cb56964235ff86cf345fcb0ac332de764baf44
Mykael Sicard <112145455+mykaelsss@users.noreply.github.com>
2026-06-07 18:51:52 -0400

    add disable to unavailable sessions

--- client/components/telemetry/SessionSidebar.tsx
+++ client/components/telemetry/SessionSidebar.tsx
@@ -193,6 +193,7 @@ export default function SessionSidebar({
                     key={s.identifier}
                     className="text-text-primary"
                     value={s.identifier}
+                    disabled={s.status !== "completed"}
                   >
                     {s.name}
                   </SelectItem>
